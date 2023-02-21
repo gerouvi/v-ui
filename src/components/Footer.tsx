f54ca92c208564db9c3b6c0c10fb17d311fa33c2
Gerard <g.roura.v@protonmail.com>
2023-02-21 09:29:45 +0000

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,9 @@
 import { Box } from '@chakra-ui/react';
 
-const Footer = () => <Box>Footer</Box>;
+const Footer = () => (
+  <Box mt="auto" mb={4}>
+    Footer
+  </Box>
+);
 
 export default Footer;
